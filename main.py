fc4a8c4b900d2e94c73c7134d4ff30823d291c56
giovanna <giovannanevesc03@gmail.com>
2025-08-12 23:29:54 -0300

    função cumprimento e chamada de nome

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print('hello world')
+
+def cumprimento(texto)
+    return f'Olá, {texto}'
+if __name__ == '__main__':
+    nome_completo = 'Giovanna Neves Candido'
+    print(cumprimento(nome_completo))
