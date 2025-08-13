c88aa74d28f854ede506a48bfe8b3b0ef41b5c9f
giovanna <giovannanevesc03@gmail.com>
2025-08-12 23:54:55 -0300

    função que sorteia 7 numeros e calcula media

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def cumprimento(texto):
     return f'Olá, {texto}'
 def media_7_numeros():
     numeros = [random.randint(1, 100) for _ in range(7)]
-    return sum(numeros) / len(numeros)
+    return numeros sum(numeros) / len(numeros)import random
+
 if __name__ == '__main__':
     nome_completo = 'Giovanna Neves Candido'
     print(cumprimento(nome_completo))
